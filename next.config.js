fcd9ea5d5ecf120b9c1860d411dfe3f85940e18e
hems <hems.inlet@gmail.com>
2020-10-09 05:46:35 +0100

    creating .env files for different environments, tweaking constants/tokenAddresses.ts to load contracts form process.env

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
-const SELECTED_ENV = process.env.ENV || 'development'
+const CURRENT_ENV = process.env.ENV || 'development'
 
-const ENVIROMENTS = {}
-
-ENVIROMENTS.development = {
-  ENV: 'development',
-  ETHEREUM_CHAIN_ID: '42'
-}
-
-ENVIROMENTS.staging = {
-  ENV: 'staging',
-  ETHEREUM_CHAIN_ID: '42'
-}
-
-ENVIROMENTS.production = {
-  ENV: 'production',
-  ETHEREUM_CHAIN_ID: '1'
-}
-
-module.exports.env = ENVIROMENTS[SELECTED_ENV]
+module.exports.env = require(`./.env.${CURRENT_ENV}.js`)
