1e788106d666dc1f90658781aa2af94f753a0229
hems <hems.inlet@gmail.com>
2021-03-09 16:01:29 +0000

    upadting aluna token address

--- .env.production.js
+++ .env.production.js
@@ -1,24 +1,24 @@
 module.exports = {
-  ENV: 'production',
-  ETHEREUM_CHAIN_ID: '1',
+  ENV: "production",
+  ETHEREUM_CHAIN_ID: "1",
 
   // contract addresses
 
-  GOVERNANCE: '0xc3859cb576f7e8a0a8537a5ce4700c95802daf81',
-  TREASURY: '0x71151b7AE78459093f1B29583f2B24aEF603cB70',
+  GOVERNANCE: "0xc3859cb576f7e8a0a8537a5ce4700c95802daf81",
+  TREASURY: "0x71151b7AE78459093f1B29583f2B24aEF603cB70",
 
   // tokens
 
   // used for boosting
-  BOOST_TOKEN: '0x3e780920601d61cedb860fe9c4a90c9ea6a35e78',
+  BOOST_TOKEN: "0x3e780920601d61cedb860fe9c4a90c9ea6a35e78",
   // usd for fetching price from coingecko
-  BOOST_TOKEN_PRODUCTION: '0x3e780920601d61cedb860fe9c4a90c9ea6a35e78',
+  BOOST_TOKEN_PRODUCTION: "0x3e780920601d61cedb860fe9c4a90c9ea6a35e78",
 
-  YCRV_TOKEN: '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8',
+  YCRV_TOKEN: "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8",
 
-  ALN_TOKEN: '0x1234',
+  ALN_TOKEN: "0x8185bc4757572da2a610f887561c32298f1a5748",
 
   // pools
-  
-  ALN_POOL_1: '0x1234'
-}
+
+  ALN_POOL_1: "0x1234",
+};
