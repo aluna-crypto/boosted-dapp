419a21875723ec3be71edc90c749610c0cbd94a2
hems <hems.inlet@gmail.com>
2021-03-17 23:36:47 +0000

    updating header

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -6,59 +6,58 @@ import { useRouter } from "next/router";
 export const Header = ({ changingRoute }) => {
   const router = useRouter();
   return (
-  <Flex
-    my="16px"
-    position="relative"
-    justifyContent="space-between"
-    alignItems="baseline"
-    width="100%"
-    bgImage={
-      router.pathname == "/about" ?
-      "url('/images/farmplants.jpg')" :
-      "url('/images/farm.jpg')"
-    } 
-    bgPos="center" bgRepeat="no-repeat"
-    bgSize="contain"
-    height="60vh"
-  >
-    <Flex flex="1" alignItems="center">
-      <Heading 
-        fontSize={"lg"} 
-        fontWeight="normal"
-        bgImage="url('/images/AlunaSymbol.svg')"
-        bgPos="left"
-        bgSize="20px auto"
-        bgRepeat="no-repeat"
-        pl="30px"
-      >  
-        ALUNA <span style={{color:"#7547dc"}}>&#10005;</span> BOOST
-      </Heading>
-      {changingRoute && <Spinner ml={4} color="grey.500" size="sm" />}
-    </Flex>
-    <Flex flex="2" justifyContent="center">
-      <NextLink href="/">
-        <Link fontSize={["sm", "lg"]} m="4" fontWeight="300">
-          HOME
+    <Flex
+      my="16px"
+      position="relative"
+      justifyContent="space-between"
+      alignItems="baseline"
+      width="100%"
+      bgImage={
+        router.pathname == "/about"
+          ? "url('/images/farmplants.jpg')"
+          : "url('/images/farm.jpg')"
+      }
+      bgPos="center"
+      bgRepeat="no-repeat"
+      bgSize="contain"
+      height="60vh"
+    >
+      <Flex flex="1" alignItems="center">
+        <Heading
+          fontSize={"lg"}
+          fontWeight="normal"
+          bgImage="url('/images/AlunaSymbol.svg')"
+          bgPos="left"
+          bgSize="20px auto"
+          bgRepeat="no-repeat"
+          pl="30px"
+        >
+          ALUNA <span style={{ color: "#7547dc" }}>&#10005;</span> FARM
+        </Heading>
+        {changingRoute && <Spinner ml={4} color="grey.500" size="sm" />}
+      </Flex>
+      <Flex flex="2" justifyContent="center">
+        <NextLink href="/">
+          <Link fontSize={["sm", "lg"]} m="4" fontWeight="300">
+            HOME
+          </Link>
+        </NextLink>
+        <Link
+          fontSize={["sm", "lg"]}
+          m="4"
+          fontWeight="300"
+          href="https://medium.com/@aluna/aln-tokenomics-fair-distribution-community-owned-self-governed-6b309fa24f49"
+          target="_blank"
+        >
+          ABOUT
         </Link>
-      </NextLink>
-      <Link
-        fontSize={["sm", "lg"]}
-        m="4"
-        fontWeight="300"
-        href="https://medium.com/@aluna/aln-tokenomics-fair-distribution-community-owned-self-governed-6b309fa24f49"
-        target="_blank"
-      >
-        ABOUT 
-      </Link>
-      {/* <NextLink href="/vote">
+        {/* <NextLink href="/vote">
         <Link fontSize={["sm", "lg"]} m="4" fontWeight="300">
           VOTE
         </Link>
       </NextLink> */}
+      </Flex>
+      <Flex flex="1" display="flex" justifyContent="flex-end"></Flex>
     </Flex>
-    <Flex flex="1" display="flex" justifyContent="flex-end">
-      
-    </Flex>
-  </Flex>
   );
-}
+};
